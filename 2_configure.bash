c0e4df6579714aeef2b0887a431e0ea060dd6a0a
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-11-26 16:37:45 -0500

    Update 2_configure.bash

--- 2_configure.bash
+++ 2_configure.bash
@@ -26,30 +26,7 @@ cd ..
 sudo cp -r temp/wordpress/ ./$DB
 sudo chown www-data:www-data "/var/www/html/$DB/" -R
 
-sudo sh -c 'echo "
-<VirtualHost *:80>
-     ServerAdmin admin@$DB
-     ServerName $DB
-     DocumentRoot /var/www/$DB
-     DirectoryIndex info.php
 
-     <Directory /var/www/$DB>
-        Options Indexes FollowSymLinks MultiViews
-        AllowOverride All
-        Order allow,deny
-        allow from all
-     </Directory>
-
-    <FilesMatch \.php$>
-        # From the Apache version 2.4.10 and above, use the SetHandler to run PHP as a fastCGI process server
-         SetHandler "proxy:unix:/run/php/php7.4-fpm.sock|fcgi://localhost"
-    </FilesMatch>
-
-     ErrorLog ${APACHE_LOG_DIR}/$DB_error.log
-     CustomLog ${APACHE_LOG_DIR}/$DB_access.log combined
-</VirtualHost>" > /etc/apache2/sites-available/$DB.conf'
-sudo a2ensite $DB
-sudo systemctl restart apache2
 
 sudo sh -c 'echo "<?php phpinfo(); ?>" > /var/www/$DB/info.php'
 

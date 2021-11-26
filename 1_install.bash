42cce9e548633a41519ff3f7c3f2059b43be0832
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-11-26 08:18:40 -0500

    Update 1_install.bash

--- 1_install.bash
+++ 1_install.bash
@@ -4,9 +4,9 @@ sudo apt upgrade
 sudo apt install tasksel
 sudo apt install unzip
 sudo apt install spice-vdagent spice-webdavd
-sudo install chromium-browser
-sudo install neovim
-sudo install git
+sudo apt install chromium-browser
+sudo apt install neovim
+sudo apt install git
 git clone https://github.com/LowTechCoder/linux-nvim
 mkdir .config/nvim/
 cp linux-nvim/init.vim .config/nvim/

c51af7bed451664f161581ce88c01875c98ef429
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-11-26 19:07:15 -0500

    Update 4_php.bash

--- 4_php.bash
+++ 4_php.bash
@@ -1,8 +1,8 @@
 # switch php versions
 # https://www.digitalocean.com/community/tutorials/how-to-run-multiple-php-versions-on-one-server-using-apache-and-php-fpm-on-ubuntu-20-04
 read -p "Enter DB: " DB;
-read -p "Enter PHP: " PHPV;
-echo "DB: $DB"
+read -p "Enter PHP Version: " PHPV;
+echo "DB Version: $DB"
 echo "PHP: $PHPV"
 read -r -p "Are these correct? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
@@ -17,10 +17,10 @@ sudo sh -c 'echo "
 <VirtualHost *:80>
      ServerAdmin admin@DB
      ServerName DB
-     DocumentRoot /var/www/DB
-     DirectoryIndex info.php
+     DocumentRoot /var/www/html/
+     DirectoryIndex index.php
 
-     <Directory /var/www/DB>
+     <Directory /var/www/html>
         Options Indexes FollowSymLinks MultiViews
         AllowOverride All
         Order allow,deny
@@ -29,17 +29,14 @@ sudo sh -c 'echo "
 
     <FilesMatch \.php$>
         # From the Apache version 2.4.10 and above, use the SetHandler to run PHP as a fastCGI process server
-         SetHandler \"proxy:unix:/run/php/phpPHPV-fpm.sock|fcgi://localhost\"
+         SetHandler "proxy:unix:/run/php/phpPHPV-fpm.sock|fcgi://localhost"
     </FilesMatch>
 
-     ErrorLog \${APACHE_LOG_DIR}/DB_error.log
-     CustomLog \${APACHE_LOG_DIR}/DB_access.log combined
+     ErrorLog ${APACHE_LOG_DIR}/DB_error.log
+     CustomLog ${APACHE_LOG_DIR}/DB_access.log combined
 </VirtualHost>" > /etc/apache2/sites-available/DB.conf'
 sudo sed -i "s#DB#$DB#g" /etc/apache2/sites-available/DB.conf
 sudo sed -i "s#PHPV#$PHPV#g" /etc/apache2/sites-available/DB.conf
 sudo mv /etc/apache2/sites-available/DB.conf "/etc/apache2/sites-available/$DB.conf"
-
-read
 sudo a2ensite $DB
-read
 sudo systemctl restart apache2

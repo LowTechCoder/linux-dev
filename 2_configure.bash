106f2d8c265acc95dffeaa5cc216cd55ecd4fc66
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-11-27 04:39:04 -0500

    Update 2_configure.bash

--- 2_configure.bash
+++ 2_configure.bash
@@ -1,8 +1,10 @@
+# https://www.digitalocean.com/community/tutorials/how-to-run-multiple-php-versions-on-one-server-using-apache-and-php-fpm-on-ubuntu-20-04
 
 #step 2
 read -p "Enter DB: " DB;
 read -p "Enter DB_USER: " DB_USER;
 read -p "Enter DB_PW: " DB_PW;
+read -p "Enter PHP Version: " PHPV;
 read -r -p "Are these correct? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
@@ -41,5 +43,44 @@ sudo cp /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
 sudo a2enconf phpmyadmin
 sudo service apache2 restart
 
-chromium "http://localhost/$DB/wp-admin/install.php" & chromium http://localhost/phpmyadmin
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update -y
+sudo apt-get install php$PHPV php$PHPV-fpm php$PHPV-mysql libapache2-mod-php$PHPV libapache2-mod-fcgid -y
+sudo systemctl start php$PHPV-fpm
+sudo a2enmod actions fcgid alias proxy_fcgi
+sudo systemctl restart apache2
+
+sudo sh -c 'echo "
+<VirtualHost *:443>
+     ServerAdmin admin@DB
+     ServerName DB
+     DocumentRoot /var/www/html/
+     DirectoryIndex index.php
+     <Directory /var/www/html>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        allow from all
+     </Directory>
+    <FilesMatch \.php$>
+        # From the Apache version 2.4.10 and above, use the SetHandler to run PHP as a fastCGI process server
+         SetHandler \"proxy:unix:/run/php/phpPHPV-fpm.sock|fcgi://localhost\"
+    </FilesMatch>
+     ErrorLog \${APACHE_LOG_DIR}/DB_error.log
+     CustomLog \${APACHE_LOG_DIR}/DB_access.log combined
+</VirtualHost>" > /etc/apache2/sites-available/DB.conf'
+sudo sed -i "s#DB#$DB#g" /etc/apache2/sites-available/DB.conf
+sudo sed -i "s#PHPV#$PHPV#g" /etc/apache2/sites-available/DB.conf
+sudo mv /etc/apache2/sites-available/DB.conf "/etc/apache2/sites-available/$DB.conf"
+sudo a2ensite $DB
+sudo a2dissite 000-default.conf
+sudo systemctl restart apache2
+
+
+
+chromium "https://localhost/$DB/wp-admin/install.php" & chromium https://localhost/phpmyadmin & chromium https://localhost/$DB/info.php&
+
+
 

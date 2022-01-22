49f5327468c3c0666ede2ec9d3ab13a5bf334480
Matt Eller <meller@affariproject.com>
2022-01-21 20:06:25 -0500

    more apache commands and permissions script

--- fix_permissions.bash
+++ fix_permissions.bash
@@ -1,8 +1,14 @@
-# change user:group
-sudo chown www-data:www-data "/var/www/html" -R
-
 # change all files to 664
-sudo find "/var/www/html" -type f -exec chmod 664 {} + 
+sudo find "/var/www" -type f -exec chmod 664 {} + 
 
 # change all folders to 775
-sudo find "/var/www/html" -type d -exec chmod 775 {} +
+sudo find "/var/www" -type d -exec chmod 775 {} +
+
+# add user to www-data
+sudo adduser $USER www-data
+
+# change user:group
+sudo chown -R www-data:www-data '/var/www'
+
+# make writable to all in group
+sudo chmod -R g+rwX '/var/www'
